47ab77257cfd5313090aa85d11333cd8184082ab
Lautaro Amengual <amengual.ln@gmail.com>
2024-01-21 21:54:39 -0300

    Active states in bottom nav icons

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,19 +1,21 @@
 import { PageWrapper } from "./PageWrapper"
 import { InvestmentsIcon, HomeIcon, DebtsIcon } from "./Icons"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export const BottomNav = () => {
+    const { pathname } = useLocation()
+
     return (
         <section className="fixed bottom-0 w-full bg-gray-50">
             <PageWrapper>
                 <div className="flex justify-evenly">
-                    <Link to="/investments" className="p-4 text-gray-400 hover:text-primary transition">
+                    <Link to="/investments" className={`p-4 text-gray-400 hover:text-primary transition ${pathname === '/investments' ? 'text-primary' : ''}`}>
                         <InvestmentsIcon />
                     </Link>
-                    <Link to="/" className="p-4 text-gray-400 hover:text-primary transition">
+                    <Link to="/" className={`p-4 text-gray-400 hover:text-primary transition ${pathname === '/' ? 'text-primary' : ''}`}>
                         <HomeIcon />
                     </Link>
-                    <Link to="/debts" className="p-4 text-gray-400 hover:text-primary transition">
+                    <Link to="/debts" className={`p-4 text-gray-400 hover:text-primary transition ${pathname === '/debts' ? 'text-primary' : ''}`}>
                         <DebtsIcon />
                     </Link>
                 </div>
